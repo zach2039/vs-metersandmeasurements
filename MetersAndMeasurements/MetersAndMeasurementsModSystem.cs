2f7e64f1cb6518cd5eac1c8d836f2364bf7152f2
Zach2039 <sumyunguy2007@gmail.com>
2023-11-15 23:47:08 -0600

    Initial commit

--- MetersAndMeasurements/MetersAndMeasurementsModSystem.cs
+++ MetersAndMeasurements/MetersAndMeasurementsModSystem.cs
@@ -3,24 +3,20 @@ using Vintagestory.API.Common;
 using Vintagestory.API.Config;
 using Vintagestory.API.Server;
 
-namespace ModTemplate
+namespace MetersAndMeasurements
 {
-    public class ModTemplateModSystem : ModSystem
+    public class MetersAndMeasurementsModSystem : ModSystem
     {
-        // Called on server and client
         public override void Start(ICoreAPI api)
         {
-            api.Logger.Notification("Hello from template mod: " + Lang.Get("mymodid:hello"));
         }
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-            api.Logger.Notification("Hello from template mod server side");
         }
 
         public override void StartClientSide(ICoreClientAPI api)
         {
-            api.Logger.Notification("Hello from template mod client side");
         }
     }
 }
